63c7f9a87261a0dddaa662816e3caaa13980be17
shlomib <shlomiphp@gmail.com>
2021-05-02 13:33:26 +0300

    on error response return loan deny

--- src/components/Body/requestLoan.js
+++ src/components/Body/requestLoan.js
@@ -1,9 +1,15 @@
 const mockServerUrl = 'http://localhost:9000/request-loan/';
 
 const requestLoan = async (amount) => {
-    const requestLoanRes = await fetch(mockServerUrl + amount);
-    const requestLoanResAsJson = await requestLoanRes.json();
-    return requestLoanResAsJson;
+    try {
+        const requestLoanRes = await fetch(mockServerUrl + amount);
+        const requestLoanResAsJson = await requestLoanRes.json();
+        return requestLoanResAsJson;
+    } catch (e) {
+        return new Promise((res) => {
+            res({isLoanApproved: false});
+        });
+    }
 };
 
 export default requestLoan;
